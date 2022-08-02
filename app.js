e221e6be46618ad41a44e23d7a0d3e7a07554ddf
DavideLopez <davidelopez7@gmail.com>
2022-08-02 18:37:53 +0200

    aggiunto genera numeri

--- app.js
+++ app.js
@@ -1,1 +1,6 @@
 console.log('simon')
+ // genera 5 numeri random compresi tra 1 e 100
+const numberGenerator = Array.from({length:5}, () => Math.floor(Math.random()* 100) + 1)
+
+console.log(numberGenerator)
+
